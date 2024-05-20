780aa42849dec2674db7885c6eea5d367da6a13b
tomohiko-urai <84952494+tomohiko-urai@users.noreply.github.com>
2024-05-20 15:46:13 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,58 +1,69 @@
 #-*- coding:utf-8 -*-
-
+import model as shinemuscat_chk 
 import sys, os
 from PIL import Image
-#import numpy as np
-
+import numpy as np
+#import cv2 
 import streamlit as st
-import cv2
-from ultralytics import YOLO
-
+#import matplotlib.pyplot as plt
+#import datetime
 
 image_size = 50
+## 2022 categories = [
+## 2022    "ng","pass" ]
+## 2022 camerapos = ["NG","GOOD"]
 
+## 20220903 categories = ["Blue","white","red"]
 
+### 20221107 categories = ["yellow","green"]
+###categories = ["yellow","green-1","green-2","green-3-4"]
+categories = ["yellow","green-1-2","green-3-4"]
+####camerapos = ["0","1","2","3"]
+camerapos = ["0","1","2"]
+### 20220903 camerapos = ["0","1","2"]
 st.set_option("deprecation.showfileUploaderEncoding", False)
 
-st.sidebar.title("シャインマスカット収穫時期判定アプリ-ai-app11")
-st.sidebar.write("画像認識モデルを使ってシャインマスカットの収穫時期の判定をします。")
+st.sidebar.title("シャインマスカット画像収穫認識アプリ")
+st.sidebar.write("オリジナルの画像認識モデルを使ってシャインマスカット収穫色判定をします。")
 
 st.sidebar.write("")
 
-#img_source = st.sidebar.radio("画像のソースを選択してください。",
-#                              ("画像をアップロード", "カメラで撮影"))
-#if img_source == "画像をアップロード":
-#    img_file = st.sidebar.file_uploader("画像を選択してください。", type=["png", "jpg"])
-#elif img_source == "カメラで撮影":
-    #img_file = st.camera_input("カメラで撮影")
-
-col1, col2 = st.columns(2)
-
-with col1:
+img_source = st.sidebar.radio("画像のソースを選択してください。",
+                              ("画像をアップロード", "カメラで撮影"))
+if img_source == "画像をアップロード":
+    img_file = st.sidebar.file_uploader("画像を選択してください。", type=["png", "jpg"])
+elif img_source == "カメラで撮影":
     img_file = st.camera_input("カメラで撮影")
 
-with col2:
 if img_file is not None:
     with st.spinner("推定中..."):
         img = Image.open(img_file)
-        #st.image(img, caption="対象の画像", width=380)
-        #st.image(img, caption="対象の画像", width=480)
+        st.image(img, caption="対象の画像", width=480)
         st.write("")
 
         img = img.convert("RGB")
-       # img = img.resize((image_size,image_size))
-        
-        model = YOLO('last.pt')
+        img = img.resize((image_size,image_size))
+        in_data = np.asarray(img)
+        X = []
+        X.append(in_data)
+        X = np.array(X)
+        # CNNのモデルを構築 --- (※3)
+        model = shinemuscat_chk.build_model(X.shape[1:])
+        model.load_weights("shinemuscat-color4-model_30_300_yellow-b_green-1-2_grenn-3-4b.hdf5")
+     　 #####model.load_weights("shinemuscat-color4-model_30_60_bk_yellowBK_.hdf5")
+        ##### 20220903 model.load_weights("tomato-color2-model2.hdf5")
+# データを予測 --- (※4)
+      
+        pre = model.predict(X)
+        y=0
+        y = pre.argmax()
+        #st.image(img, caption="対象の画像", width=480)
+        #st.write("")
 
-        ret = model(img,save=True, conf=0.4, iou=0.1)
-#        ret = model(img,save=True, conf=0.6, iou=0.1)
-        annotated_frame = ret[0].plot(labels=True,conf=True)
-        annotated_frame = cv2.cvtColor(annotated_frame , cv2.COLOR_BGR2RGB)
-        
-    
+        # 予測
+        #results = predict(img)
+
         # 結果の表示
         st.subheader("判定結果")
-        st.image(annotated_frame, caption='出力画像',width=380) 
-        #st.image(annotated_frame, caption='出力画像') 
-        #st.write(camerapos[y] + "です。")
-        #st.write(categories[y] + "です。")
+        st.write(camerapos[y] + "です。")
+        st.write(categories[y] + "です。")
